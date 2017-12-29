fbe9decf4ce5b96b2d38938916e1b77541837f23
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2017-12-29 11:44:32 +0100

    Refactored the scripts

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,7 +1,7 @@
 ln -s \
     /usr/local/lib/python3.6/site-packages/cv2.cpython-36m-x86_64-linux-gnu.so \
-    /home/alex/.conda/envs/opencv_build/lib/python3.6/site-packages/cv2.so
-    
+    $HOME/.conda/envs/opencv_build/lib/python3.6/site-packages/cv2.so
+
 ln -s \
     /usr/local/lib/python3.6/site-packages/cv2.cpython-36m-x86_64-linux-gnu.so \
-    /home/alex/.conda/envs/ml36/lib/python3.6/site-packages/cv2.so
+    $HOME/.conda/envs/ml36/lib/python3.6/site-packages/cv2.so
