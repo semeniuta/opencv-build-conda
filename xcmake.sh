d21aec005585c0cb11352c9a9317106450b04681
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2018-09-02 13:58:38 +0200

    Improved xcmake.sh to work across different platforms and fully depend on conda Python

--- xcmake.sh
+++ xcmake.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
-CONDA_ENV_PATH=$HOME/.conda/envs/
+CONDA_ENV_PATH=$HOME/miniconda3/envs
 CONDA_ENV_NAME=opencv_build
-WHERE_OPENCV=../opencv
-WHERE_OPENCV_CONTRIB=../opencv_contrib
+WHERE_ENV=$CONDA_ENV_PATH/$CONDA_ENV_NAME
 
-cmake -D CMAKE_BUILD_TYPE=RELEASE \
+echo "Starting CMake with conda environment: $WHERE_ENV"
+
+cmake \
+    -D ENABLE_CXX11=ON \
+	-D CMAKE_BUILD_TYPE=RELEASE \
+	-D OPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules \
 	-D CMAKE_INSTALL_PREFIX=/usr/local \
-	-D PYTHON3_EXECUTABLE=$CONDA_ENV_PATH/$CONDA_ENV_NAME/bin/python \
+	-D PYTHON3_EXECUTABLE=$WHERE_ENV/bin/python \
+	-D PYTHON3_PACKAGES_PATH=$WHERE_ENV/lib/python3.6/site-packages \
+	-D PYTHON3_LIBRARIES=$WHERE_ENV/lib/libpython3.6m.so \
+	-D PYTHON3_INCLUDE_DIRS=$WHERE_ENV/include/python3.6m \
+	-D BUILD_opencv_python2=OFF \
+	-D BUILD_opencv_python3=ON \
 	-D INSTALL_C_EXAMPLES=ON \
 	-D INSTALL_PYTHON_EXAMPLES=ON \
-	-D OPENCV_EXTRA_MODULES_PATH=$WHERE_OPENCV_CONTRIB/modules \
 	-D BUILD_EXAMPLES=ON \
-	$WHERE_OPENCV
+	../opencv 
